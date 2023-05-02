2c030cf34a3d027cc8e6437083019c5c9df37e0c
KayqueMBM <kayque_moreira@estudante.sesisenai.org.br>
2023-05-02 08:59:46 -0300

    formulario

--- crud.php
+++ crud.php
@@ -1,17 +1,33 @@
 <?php
-include("conecta.php");
+    include("conecta.php");
 
-$matricula = $_POST["matricula"];
-$nome = $_POST["nome"];
-$idade = $_POST["idade"];
+    $matricula = $_POST["matricula"];
+    $nome = $_POST["nome"];
+    $idade = $_POST["idade"];
 
-if(isset($_POST["gravar"]))
-{
-    $comando= $pdo->prepare("INSERT INTO alunos(Nome,Idade) VALUES('$nome',$idade)");
-}
+    if(isset($_POST["gravar"])){   
+        $comando = $pdo->prepare("INSERT INTO aluno Values($matricula,'$nome',$idade)");
+        $resultado = $comando->execute();
+        header("location: cadastro.html");
+    }
+    if(isset($_POST["excluir"])){
+        $comando = $pdo->prepare("DELETE FROM aluno where matricula=$matricula");
+        $resultado = $comando->execute();
+        header("location: cadastro.html");
+    }
+    if(isset($_POST["alterar"])){
+        $comando = $pdo->prepare("UPDATE FROM aluno SET nome='$nome',idade=$idade WHERE matricula=$matricula");
+        $resultado = $comando->execute();
+        header("location: cadastro.html");
+    }
 
-$resultado = $comando->execute();
+    if(isset($_POST["listar"])){
+        $comando = $pdo->prepare("SELECT * FROM aluno");
+        $resultado = $comando->execute();
+        header("location: cadastro.html");
 
-//Para voltar no formulário:
-    header("location: cadastro.html");
+        while($linhas = $comando->fetch()){
+            
+        }
+    }    
 ?>
